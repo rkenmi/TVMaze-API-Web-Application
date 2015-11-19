370cf1f7cb3d31c3e9539ba1f91746317dfb1e58
rkenmi <rkenmi@gmail.com>
2015-11-19 15:55:36 -0800

    re-organized directory structure; api/routes.js, client/controller.js, client/index.html, client/searchResult.html

---
+++ api/routes.js
@@ -0,0 +1,23 @@
+var config = require('../config.json');
+var superagent = require('superagent');
+
+module.exports = function(app) {
+
+  app.get('/api/shows/:leg', function(req, res){
+    config.TVMaze.key = req.params.leg;
+    superagent.get(config.TVMaze.url + config.TVMaze.key)
+      .end(function (err, response) {
+        if(err) console.error("Cannot retrieve url");
+        else{
+          if(response && response.body){
+            var arr = [];
+            for(var i = 0; i < response.body.length; i++){
+              arr.push(response.body[i].show);
+            }
+            res.send(arr);
+          }
+        }
+    });
+  });
+
+};
--- server.js
+++ server.js
@@ -3,32 +3,14 @@ var superagent = require('superagent');
 var app = express();
 
 
-app.use(express.static(__dirname + '/public')); // Set the web root to be /public
+app.use(express.static('./client')); // Set the web root to be ./client
 
+require('./api/routes')(app);
 
-app.get('/', function (req, res) {
-	res.sendFile('./public/index.html');
-});
-
-app.get('/api/shows/:leg', function(req, res){
-  console.log(req.params.leg);
-  var search = req.params.leg;
-  var TVMazeURL = 'http://api.tvmaze.com/search/shows?q=' + search;
-  superagent.get(TVMazeURL)
-    .end(function (err, response) {
-      if(err) console.error("HALP PLS");
-      else{
-        if(response && response.body){
-          var arr = [];
-          for(var i = 0; i < response.body.length; i++){
-            arr.push(response.body[i].show);
-          }
-          res.send(arr);
-        }
-      }
-  });
+app.get('*', function (req, res) {
+	res.sendFile('./public/index.html', { root: __dirname });
 });
 
 app.listen(8080, function () {
-	console.log('Server is running.')
+	console.log('Server is running on 8080.')
 });
